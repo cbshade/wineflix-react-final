94df44150d21fd226f50b34daa9c8709d6dd318d
Chris Shade <cbshade@gmail.com>
2023-06-05 16:58:29 -0700

    Replaced first nav item.

---
+++ src/NavigationLink.js
@@ -0,0 +1,9 @@
+const NavigationLinks = () => {
+    return (
+        <li className="active">
+            <a href="https://developer.mozilla.org">Home</a>
+        </li>
+    )
+}
+
+export default NavigationLinks
